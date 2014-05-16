7a4e5e7ac39f548c84003198e6712ba6933d96f2
Ayush Goel <ayush.g@directi.com>
2014-05-16 10:30:19 +0530

    Remove redeclaration of functions

--- Ex3.73.rkt
+++ Ex3.73.rkt
@@ -2,12 +2,6 @@
 
 (require "stream-memoized.rkt")
 
-(define (add-streams s1 s2)
-  (stream-map + s1 s2))
-
-(define (scale-stream stream factor)
-  (stream-map (lambda (x) (* x factor)) stream))
-
 (define (integral integrand initial-value dt)
   (define int
     (cons-stream initial-value
